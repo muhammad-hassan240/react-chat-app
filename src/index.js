89b92e34ebc0fb3fe60d24d79bf5912fa9bb300f
Hassan A <hassanhussain31202@gmail.com>
2023-05-24 07:51:39 +0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import ChatWrapper from "./chatWrapper";
+import "./index.css";
+
+
+
+ReactDOM.render(
+  <ChatWrapper />,
+  document.getElementById('root')
+);
